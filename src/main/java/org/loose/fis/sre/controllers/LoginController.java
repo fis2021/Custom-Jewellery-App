e5f5c493dc84cb8c03485de04999f7a40ead7362
ComanAlex <comanalex33@yahoo.com>
2021-04-05 12:30:36 +0300

    Login interface and Login Controller added

---
+++ src/main/java/org/loose/fis/sre/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package org.loose.fis.sre.controllers;
+
+public class LoginController {
+
+    public void handleLoginAction() {
+        //TODO
+    }
+
+    public void handleCreateAccountAction() {
+        //TODO
+    }
+}
