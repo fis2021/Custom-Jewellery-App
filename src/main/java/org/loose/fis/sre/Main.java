a283296ee4f7b5c94efda193426bf84bc9a9bb33
ComanAlex <comanalex33@yahoo.com>
2021-04-05 19:30:43 +0300

    "Create Account" button functionality added

--- src/main/java/org/loose/fis/sre/Main.java
+++ src/main/java/org/loose/fis/sre/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
         UserService.initDatabase();
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
         primaryStage.setTitle("Registration Example");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
--- src/main/java/org/loose/fis/sre/controllers/LoginController.java
+++ src/main/java/org/loose/fis/sre/controllers/LoginController.java
@@ -1,12 +1,37 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+
 public class LoginController {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button createAccountButton;
+
+    @FXML
+    private Text registrationMessage;
+
+
 
     public void handleLoginAction() {
         //TODO
     }
 
-    public void handleCreateAccountAction() {
+    public void handleCreateAccountAction() throws Exception{
         //TODO
+        Stage primary = (Stage) loginButton.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
+        Scene nextScene = new Scene(root, 800, 600);
+
+        primary.setScene(nextScene);
     }
 }
