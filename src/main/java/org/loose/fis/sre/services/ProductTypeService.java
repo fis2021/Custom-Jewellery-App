dfdda2a93d00f40ecf68ee798aa65f7c93da8da3
ComanAlex <comanalex33@yahoo.com>
2021-04-12 17:26:31 +0300

    InitDatabase method for ProductType database initialization added

--- src/main/java/org/loose/fis/sre/services/ProductTypeService.java
+++ src/main/java/org/loose/fis/sre/services/ProductTypeService.java
@@ -1,4 +1,23 @@
 package org.loose.fis.sre.services;
 
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+import org.loose.fis.sre.model.ProductType;
+import org.loose.fis.sre.model.User;
+
+import static org.loose.fis.sre.services.FileSystemService.getPathToFile;
+
 public class ProductTypeService {
+
+    private static ObjectRepository<ProductType> productTypeRepository;
+
+    public static void initDatabase() {
+        Nitrite database = Nitrite.builder()
+                .filePath(getPathToFile("product-type.db").toFile())
+                .openOrCreate("test", "test");
+
+        productTypeRepository = database.getRepository(ProductType.class);
+    }
+
+    
 }
