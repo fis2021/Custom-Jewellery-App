be76dbc67d5b64220b2f87f91e2bc8059c2513ff
ComanAlex <comanalex33@yahoo.com>
2021-04-12 21:04:07 +0300

    initDatabase method added for Material database initialization added

--- src/main/java/org/loose/fis/sre/services/MaterialService.java
+++ src/main/java/org/loose/fis/sre/services/MaterialService.java
@@ -1,4 +1,20 @@
 package org.loose.fis.sre.services;
 
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+import org.loose.fis.sre.model.Material;
+
+import static org.loose.fis.sre.services.FileSystemService.getPathToFile;
+
 public class MaterialService {
+
+    private static ObjectRepository<Material> materialRepository;
+
+    public static void initDatabase() {
+        Nitrite database = Nitrite.builder()
+                .filePath(getPathToFile("materials.db").toFile())
+                .openOrCreate("test", "test");
+
+        materialRepository = database.getRepository(Material.class);
+    }
 }
