f8099214a3e6c5e1c295d288fa328348575e506c
CostinValentin <costin_valy2000@yahoo.com>
2021-04-13 14:46:59 +0300

    ViewProductsManagerController class functionality complete

--- src/main/java/org/loose/fis/sre/controllers/ViewProductsManagerController.java
+++ src/main/java/org/loose/fis/sre/controllers/ViewProductsManagerController.java
@@ -1,4 +1,44 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.loose.fis.sre.model.Material;
+import org.loose.fis.sre.model.ProductType;
+import org.loose.fis.sre.services.MaterialService;
+import org.loose.fis.sre.services.ProductTypeService;
+
 public class ViewProductsManagerController {
+
+    @FXML
+    private TableView<ProductType> typeTable;
+    @FXML
+    private TableColumn<ProductType, String> productTypeColumn;
+    @FXML
+    private TableColumn<ProductType, Integer> productPriceColumn;
+
+    @FXML
+    private TableView<Material> materialTable;
+    @FXML
+    private TableColumn<Material, String> materialNameColumn;
+    @FXML
+    private TableColumn<Material, Integer> materialPriceColumn;
+
+    public void initialize() {
+        productTypeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+        productPriceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        typeTable.setItems(types);
+
+        materialNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        materialPriceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        materialTable.setItems(materials);
+    }
+
+    private ObservableList<ProductType> types = FXCollections.observableArrayList(ProductTypeService.productTypes());
+    private ObservableList<Material> materials = FXCollections.observableArrayList(MaterialService.materials());
 }
